66646497c9dfe517c4aee8b06e54d135c0bbea8c
zjczdzs <1276836148@qq.com>
2023-05-22 10:39:38 +0800

    Initial commit

---
+++ src/main/java/com/example/zhe/ZHeApplication.java
@@ -0,0 +1,13 @@
+package com.example.zhe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZHeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZHeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zhe/ZHeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zhe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZHeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
