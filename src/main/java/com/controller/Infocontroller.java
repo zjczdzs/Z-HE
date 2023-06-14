55a16037aae687fa970d9df19dd2de0e93af2fcd
zjczdzs <1276836148@qq.com>
2023-06-14 18:36:05 +0800

    接入flume与推荐算法，待接入监控与用户认证加密。

--- src/main/java/com/controller/Infocontroller.java
+++ src/main/java/com/controller/Infocontroller.java
@@ -1,8 +1,14 @@
 package com.controller;
 
+import Utils.EsUtil;
+import Utils.FlumeUtil;
+import Utils.HBaseUtil;
 import Utils.HdfsUtil;
 import com.Dao.Flag;
-import com.service.InfoService;
+import com.Dao.Info;
+import com.Dao.Result;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,24 +16,57 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Map;
 
 @RestController
 public class Infocontroller {
-    private InfoService infoService;
+    static final Logger logger = LoggerFactory.getLogger(Infocontroller.class);
 
-//    @RequestMapping("test")
-//    public Flag Testmapping() {
-//        Flag flag = new Flag();
-//        flag.setInformation("goodgood");
-//        return flag;
-//    }
+    //    post请求
+//    @RequestBody 表示接收请求是JSON格式的数据
+    @PostMapping(value = "/ESsearch", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public Info[] SearchFlag(@RequestParam String flag, HttpServletRequest request){
+        //先写用户搜索日志
+        writetolog_h("Search"+flag,request);
+        writetolog_f("Search"+flag,request);
+        Info[] map = new Info[0];
+        //int count;
+        try {
+            map = EsUtil.search(flag,"article");
+            // count = map.length;
+        } catch (Exception e) {
+            logger.error("查询索引错误!{}",e);
+            e.printStackTrace();
+        }
+        return map;
+    }
 
-    //post请求
-    //@RequestBody 表示接收请求是JSON格式的数据
-    @PostMapping(value = "/search", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    public Flag SearchFlag(@RequestParam String flag, HttpServletRequest request){
+
+    /**
+     * 查看文章详细信息
+     * @return
+     */
+    @RequestMapping(value = "/HBsearch", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public Result detailArticleById(@RequestParam String id,HttpServletRequest request){
+        Result result = new Result();
+        try{
+            Map<String, String> map = HBaseUtil.getFromHBase("article", id);
+            result.setTitle(map.get("title"));
+            result.setAuthor(map.get("author"));
+            result.setContent(map.get("content"));
+            //写入日志
+            System.out.println("read"+map.get("title"));
+            writetolog_h("read"+map.get("title"),request);
+            writetolog_f("read"+map.get("title"),request);
+        }catch (Exception e){
+            logger.error("HBase数据查询异常："+e.getMessage());
+        }
+        return result;
+    }
+
+    public void writetolog_h(String action, HttpServletRequest request) {
+        //先写用户搜索日志
         Flag writertolog = new Flag();
-
         // 获取系统时间
         LocalDateTime datetime = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
@@ -36,7 +75,7 @@ public class Infocontroller {
         String clientIp = request.getRemoteAddr();
         writertolog.setDate(formattedDatetime);
         writertolog.setIp(clientIp);
-        writertolog.setInformation(flag);
+        writertolog.setInformation(action);
         try {
             HdfsUtil hdfsUtil = new HdfsUtil();
             hdfsUtil.init();
@@ -45,10 +84,26 @@ public class Infocontroller {
         } catch (IOException e) {
             System.out.println("日志写入hdfs发生异常：" + e.getMessage());
         }
-
-
-
-        return writertolog;
+    }
+    public void writetolog_f(String action, HttpServletRequest request) {
+        // 先写用户搜索日志
+        Flag writertolog = new Flag();
+        // 获取系统时间
+        LocalDateTime datetime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formattedDatetime = datetime.format(formatter);
+        // 获取客户端 IP 地址
+        String clientIp = request.getRemoteAddr();
+        writertolog.setDate(formattedDatetime);
+        writertolog.setIp(clientIp);
+        writertolog.setInformation(action);
+        try {
+            FlumeUtil.init();
+//            System.out.println(writertolog);
+            FlumeUtil.sendLogToFlume(writertolog.toString());
+        } catch (IOException e) {
+            System.out.println("日志写入 Flume 发生异常：" + e.getMessage());
+        }
     }
 
 }
